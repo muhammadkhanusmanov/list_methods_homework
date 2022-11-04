bf4a63c6d9ca43fc109130d26a75ea148d8a10ad
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-11-04 06:45:10 +0500

    5-masala

--- List05.py
+++ List05.py
@@ -9,4 +9,6 @@ def main(numbers1, numbers2):
     Returns:
         list: return answer
     """
-    return
+    numbers2.insert(0,numbers1.pop())
+    numbers=numbers1+numbers2
+    return numbers
